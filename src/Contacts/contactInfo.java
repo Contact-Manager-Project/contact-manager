0353146f65be997e1c7b00d375cab9a4fd9b61b8
Micah Wood <micahswood7@gmail.com>
2022-03-17 12:01:50 -0500

    addContact function been added

--- src/Contacts/contactInfo.java
+++ src/Contacts/contactInfo.java
@@ -39,8 +39,12 @@ public class contactInfo {
 //        System.out.printf("Name | PhoneNumber\n ----------\n %s", contact);
 //    }
 
-    private static void showContacts() {
-
+    private static void showContacts(List<String> info) {
+        System.out.println("Name | Phonenumber");
+        System.out.println("-------");
+        for (int i = 0; i < info.size(); i++) {
+            System.out.println(info.get(i));
+        }
     }
 
     public static List<Contact> stringToObject(List<String> contactStrings) {
@@ -58,24 +62,30 @@ public class contactInfo {
         return createdObject;
     }
 
+    public static List<Contact> addContact(List<Contact> contactList) {
+        Scanner firstInput = new Scanner(System.in);
+        Scanner secondInput = new Scanner(System.in);
+        System.out.println("Please enter in a name: ");
+        String name = firstInput.nextLine();
+        System.out.println("Please enter in a phone number: ");
+        String phoneNumber = secondInput.nextLine();
+        Contact newContact = new Contact(name, phoneNumber);
+        contactList.add(newContact);
+        return contactList;
+    }
+
     public static void main(String[] args) {
         printMenu();
 //        addNewContact();
-        showContacts();
 
         String directory = "contactBook";
         String filename = "contacts.txt";
         Path contactDirectory = Paths.get(directory);
         Path contactFile = Paths.get(directory, filename);
         List<String> info = readFile(contactFile);
-        System.out.println(stringToObject(info));
-        System.out.println(info);
+        List<Contact> contactList = stringToObject(info);
 
         try{
-
-
-
-
 
             if (Files.notExists(contactDirectory)){
                 Files.createDirectories(contactDirectory);
@@ -85,18 +95,28 @@ public class contactInfo {
                 Files.createFile(contactFile);
             }
 
-            List<String> contact = new ArrayList<>(Arrays.asList("Sonic the Hedgehog | 76642464" , "Scooby-Doo the Brown Dog | 6978379 ", "Fred the Caveman Flintstone | 35468786637 ", "the Alternate TV Intruder | 6163351"));
-            Files.write(contactFile, contact);
+//            List<String> contact = new ArrayList<>(Arrays.asList("Sonic the Hedgehog | 76642464" , "Scooby-Doo the Brown Dog | 6978379 ", "Fred the Caveman Flintstone | 35468786637 ", "the Alternate TV Intruder | 6163351"));
+//            Files.write(contactFile, contact);
 
+            String inputString;
             do {
-                if (input.nextInt() == 1){
-
+                inputString = input.nextLine();
+                if (inputString.equals("1")){
+                    showContacts(info);
+                }
+                if (inputString.equals("2")) {
+                    contactList = addContact(contactList);
+                }
+                if (inputString.equals("3")){
 
                 }
-                if (input.nextInt() == 2) {
-//addNewContact();
+                if (inputString.equals("4")){
+
                 }
-            }while (input.nextInt() != 5);
+                if (inputString.equals("5")){
+                    System.out.println("Good Bye!");
+                }
+            }while (!inputString.equals("5"));
 
 
         } catch (IOException e) {
